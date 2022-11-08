26020935ab62d286112122273f185f2809c7a0c1
Guadalupe Yañez Cosquilla <mariabelem92@hotmail.com>
2022-11-07 20:46:19 -0600

    1.- Filtrar Panel de habitaciones - GYC

--- pms/tests.py
+++ pms/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
+from unittest.mock import Mock
+from pms.models import Room
+
 # Create your tests here.
+# RUN TEST ./manage.py test pms.tests.RoomFilterTestCase
+
+class RoomFilterTestCase(TestCase):
+    def setUp(self):
+        pass
+    
+    def test_filter_byname(self):
+        data = {'name':'Room 1'}
+        rooms =  Mock(spec=Room.objects)
+        rooms.filter().return_value =  data.get('name')
+        self.assertIsNotNone(rooms)
+
+
+        
+
